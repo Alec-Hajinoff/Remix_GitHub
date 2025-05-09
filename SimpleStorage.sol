6227c76a59018983af41e9290d58319aaaba52b8
Alec Hajinoff <hajinoff@gmail.com>
2025-05-09 16:56:05 +0100

    9.5.25, 16.56

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18; //stating our version
+pragma solidity 0.8.30; //stating our version
 
 contract SimpleStorage {
     int256 public favoriteNumber;
     function store(int256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
-    //0xd9145CCE52D386f254917e481eB44e9943F39138
+    function retrieve() public view returns (int256) {
+         return favoriteNumber;
+    }
 }
+
+//*a view function that can be accessed only by the current contract
+contract One {
+    uint256 someNumber;
+
+    // A private view function that can only be accessed by this contract
+    function getNumber() private view returns (uint256) {
+        return someNumber;
+    }
+
+    function callPrivateFunction() public view returns (uint256) {
+        return getNumber(); // This function can call the private one
+    }
+}
+
