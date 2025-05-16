d193dfc94d7284b91457c694b5dbc0c7a641482b
Alec Hajinoff <hajinoff@gmail.com>
2025-05-16 07:34:40 +0100

    16.5.25, 7.34

--- FundMe.sol
+++ FundMe.sol
@@ -6,14 +6,25 @@
 
 pragma solidity ^0.8.0;
 
-contract FundMe {
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+contract FundMe {
+    uint256 public minimumUsd = 5;
     function fund() public payable {
-        require(msg.value > 1e18);
+        require(msg.value > minimumUsd);
     }
 
     //function withdraw() public {
-
     //}
+
+    function getPrice() public {
+        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        priceFeed.getChainlinkDataFeedLatestAnswer();
+    }
+
+    function getConversionRate() public {
+    }
 
 }
