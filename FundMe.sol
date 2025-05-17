e2f6266da1ea323e83658c27a7d7bdd077c2ddef
Alec Hajinoff <hajinoff@gmail.com>
2025-05-17 07:32:56 +0100

    17.5.25, 7.32

--- FundMe.sol
+++ FundMe.sol
@@ -9,22 +9,31 @@ pragma solidity ^0.8.0;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5e18;
     function fund() public payable {
-        require(msg.value > minimumUsd);
+        require(getConversionRate(msg.value) >= minimumUsd, "You must request more than 5 USD");
     }
 
     //function withdraw() public {
     //}
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         //ABI
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        priceFeed.getChainlinkDataFeedLatestAnswer();
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        //Price of ETH in terms of USD
+        return uint256(price * 1e10);
     }
 
-    function getConversionRate() public {
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 
+    function getVersion() public view returns (uint256) {
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    return priceFeed.version();
+}
 }
