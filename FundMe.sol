f77d2cdf69bff81f6cc8a798ca3f9c31a1e22622
Alec Hajinoff <hajinoff@gmail.com>
2025-05-19 07:29:29 +0100

    19.5.25, 7.29

--- FundMe.sol
+++ FundMe.sol
@@ -3,20 +3,21 @@
 // Set a minimum funding value in USD
 
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
-
 import {PriceConverter} from "./PriceConverter.sol";
 
+error notOwner();
+
 contract FundMe {
-
     using PriceConverter for uint256;
-
     uint256 public minimumUsd = 5e18;
-
     address[] public funders;
-
     mapping (address => uint256) public addressToAmountFunded;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "You must request more than 5 USD");
@@ -24,12 +25,11 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
-
         funders = new address[](0);
         payable(msg.sender).transfer(address(this).balance);
         bool sendSuccess = payable(msg.sender).send(address(this).balance);
@@ -37,4 +37,19 @@ contract FundMe {
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require (callSuccess, "Call failed");
     }
+
+    modifier onlyOwner() {
+        //require(msg.sender == owner, "Only owner can withdraw funds.");
+        if(msg.sender != owner) { revert notOwner(); }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
 }
